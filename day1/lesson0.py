1343786d6228e6e54c2c700c2e28034999df4d51
lukafifia123 <lukafifia1979@gmail.com>
2024-10-07 20:21:56 +0400

    first commit

---
+++ day1/lesson0.py
@@ -0,0 +1,1 @@
+print("day 0 homework")
---
+++ day1/lesson1.py
@@ -0,0 +1,49 @@
+
+
+#we want to paint a house
+
+
+#step 1: draw a square
+
+from turtle import *
+
+speed(30)
+width(7)
+color("purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+
+#drawing a door
+
+forward(70)
+color("yellow")
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+
+
+exitonclick()
