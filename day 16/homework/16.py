fcddd76ecf4f76f2d51beeced616dc310cad332d
lukafifia123 <lukafifia1979@gmail.com>
2024-12-21 18:52:56 +0400

    Add files via upload

---
+++ day 16/homework/16.py
@@ -0,0 +1,68 @@
+counter = 0
+while counter < 50:
+    print("GOA BEST!!!")
+    counter += 1
+
+    for counter in range(50):
+    print ("GOA BEST")
+
+
+    counter = 1
+
+    while counter <= 10:
+    print(counter)
+    counter += 1 
+
+
+
+
+    counter = 10
+
+while counter >= 1:
+    print(counter)
+    counter -= 1  
+
+    print("Blast off!")
+
+
+    
+correct_password = "mysecretpassword"
+
+
+password = input("wrongpassword: ")
+
+while password != correct_password:
+    print("anotherwrongpassword.")
+    password = input("mysecretpassword: ")
+
+print("პაროლი სწორია! დაშვება წარმატებით განხორციელდა.")
+
+
+
+
+
+
+correct_username = "user123"
+correct_password = "mysecretpassword"
+
+username = input("wronguser: ")
+password = input("wrongpassword: ")
+
+while username != correct_username or password != correct_password:
+    print("wronguser wrongpassword.")
+    username = input("user123: ")
+    password = input("mysecretpassword: ")
+
+print("მომხმარებლის სახელი და პაროლი სწორი! დაშვება წარმატებით განხორციელდა.")
+
+
+number = int(input("5 : "))
+
+while number > 5:
+    factorial *= number  
+    number -= 5 
+
+    print(f"5: {factorial}")
+
+    
+
